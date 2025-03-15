d0397510acf9503aa0200e37866080c8f33d117a
zensible test 1 <75108159+zensibletest1@users.noreply.github.com>
2025-03-15 16:51:03 +0530

    Add files via upload

--- hangman.py
+++ hangman.py
@@ -1,5 +1,8 @@
+from hangman_wordlist import words
+import random
+
 body_parts=['O','-','|','^']
-word=['a','l','a','s','k','a']
+# word=['a','l','a','s','k','a']
 def display_game(body_parts):  
     print(' -------')
     print('|       |')
@@ -14,38 +17,54 @@ def display_game(body_parts):
 display_game(body_parts)
 
 def user_answer(body_parts):
+    # country, capital = random.choice(list(d.items()))
+    body_parts_copy=body_parts[:]
+    word,description=random.choice(list(words.items()))
+    word_list=list(word.lower())
+    word_len=len(word)
     print('\nFind the word')
     # print(''.join(word))
-    print('A state in United States')
+    print(f'{description} The word length is {len(word)}')
     lives=4
     answer=''
     while lives>=1:
         print(f'{lives} live/s remaining')
-        answer=input('Letter: ')
+        answer=input('Letter: ').lower()
        
-        if answer in word:
-            word.remove(answer)
-            if word==[]:
-                return 'Great, all the letters are correct!'
+        if answer in word_list:
+            word_len-=1
+            word_list.remove(answer)
+            if word_len!=0:
+                print(f'Correct! {word_len} more letter/s to go!')
+            if word_list==[]:
+                return f'Great, the word is {word.upper()}.All the letters are correct!'
             
                 
         else:
             lives-=1
-            body_parts.pop()
-            display_game(body_parts) 
-            if body_parts==[]:
-                return 'OOps, you failed the game!'  
+            body_parts_copy.pop()
+            display_game(body_parts_copy) 
+            if body_parts_copy==[]:
+                return f'OOps, you failed the game! The word is {word.upper()}'  
      
 
 def main():
-    print(user_answer(body_parts))
+    game_on=True
+    while game_on:
+        print(user_answer(body_parts))
+        user_input=''
+        while True:
+            user_input=input('Do you want to keep playing? (y/n) ').lower()
+            if user_input=='y' or user_input=='n':
+                break
+            print('Please enter \'y\' or \'n\' ')
+        if user_input=='n':
+            game_on=False    
+        
+                
+    print('Thank you for playing...see ya!')        
+    
        
 if __name__=='__main__':
     main()    
           
- 
-     
-
-
- 
-        
---
+++ hangman_wordlist.py
@@ -0,0 +1,19 @@
+words={
+    'Alaska':'A state in United States',
+    'Mars':'A planet in galaxy',
+    'Redbull':'An energy drink',
+    'Austria':'Country in europe',
+    'Tibet':'Country which loves Tsampa',
+    'Holi':'Indian festival',
+    'Losar':'Term for Tibetan new year',
+    'Dosa':'South indian dish',
+    'Lhasa':'Capital city of Tibet',
+    'Elon':'CEO of SpaceX Tesla...',
+    'Trump':'President of United States',
+    'Modi':'Prime minister of India',
+    'Pasta':'Famous dish in Italy',
+    'Stanford':'College in Palo Alto',
+    'Hockey':'Famous sport in India',
+    'Bangalore':'Sillicon Valley of India',
+    
+}
